f47667e0db7ade4eb0415e0dd2434a8910db2591
night-glider <85761149+night-glider@users.noreply.github.com>
2023-04-28 18:03:27 +0300

    Create dialogue_label.gd

---
+++ addons/dialogue_label/dialogue_label.gd
@@ -0,0 +1,12 @@
+@tool
+extends EditorPlugin
+
+
+func _enter_tree():
+	print("thank you for downloading Dialogue Label!")
+	pass
+
+
+func _exit_tree():
+	# Clean-up of the plugin goes here.
+	pass
